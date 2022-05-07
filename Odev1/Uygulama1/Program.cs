1390761fb3395741810e7898c983644ea90682ea
Hakan <ahmethakanozturk34@gmail.com>
2022-05-07 16:30:46 +0300

    Uygulama 1

---
+++ Odev1/Uygulama1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = ReadNumber("Type how many numbers you want to enter: ");
+            int[] array = new int[n];
+            for(int i = 0; i < n; i++)
+            {
+                array[i] = ReadNumber("Enter " + (i + 1) + ". number: ");
+            }       
+            foreach(var i in array)
+            {
+                if(i % 2 == 0) Console.WriteLine(i);
+            }
+        }
+
+        static int ReadNumber(string msg)
+        {
+            while(true)
+            {
+                Console.Write(msg);
+                int n;
+                try
+                {
+                    n = Convert.ToInt32(Console.ReadLine());
+                    return n;
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine("Please just enter a number!");
+                }
+            }
+        }
+    }
+}
