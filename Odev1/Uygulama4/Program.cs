d80a92327cec8218b741339fb78c31ecb6a20099
Hakan <ahmethakanozturk34@gmail.com>
2022-05-07 21:17:24 +0300

    Uygulama 4

---
+++ Odev1/Uygulama4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a sentence: ");
+            string input = Console.ReadLine();
+            int letterCount = 0, wordCount = 1;
+            bool isLastLetter = true;
+            for(int i = 0; i < input.Length; i++)
+            {
+                if(input[i] != ' ')
+                {
+                    isLastLetter = true;
+                    letterCount++;
+                }
+                if(isLastLetter && input[i] == ' ')
+                {
+                    isLastLetter = false;
+                    wordCount++;
+                }
+            }
+
+            Console.WriteLine("Letter count: " + letterCount);
+            Console.WriteLine("Word count: " + wordCount);
+        }
+    }
+}
