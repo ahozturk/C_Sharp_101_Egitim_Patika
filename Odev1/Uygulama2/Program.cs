c5649a14c185051d956f24b903f934a811b2a93c
Hakan <ahmethakanozturk34@gmail.com>
2022-05-07 19:46:10 +0300

    Uygulama 2_1

--- Odev1/Uygulama2/Program.cs
+++ Odev1/Uygulama2/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                int n = ReadNumber("Please enter how many you want to enter: ");
+                if (n < 1)
+                {
+                    Console.WriteLine("You selected min value (1)");
+                    n = 1;
+                }
+                
+                int m = ReadNumber("Please enter your referance number: ");
+                int[] array = new int[n];
+
+                for(int i = 0; i < n; i++)
+                {
+                    array[i] = ReadNumber("Enter" + (i + 1) + ". number: ");
+                }
+
+                foreach(var i in array)
+                {
+                    if(i % m == 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+        }
+        static int ReadNumber(string msg)
+        {
+            while(true)
+            {
+                Console.Write(msg);
+                int n;
+                try
+                {
+                    n = Convert.ToInt32(Console.ReadLine());
+                    return n;
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine("Please just enter a number!");
+                }
+            }
+        }
+    }
+
+    
+}
