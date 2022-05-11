39f39380438794d66aa0f53518415bd8409005cb
Hakan <ahmethakanozturk34@gmail.com>
2022-05-11 21:45:37 +0300

    Collections2

---
+++ Koleksiyonlar2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList();
+            list.Add("Hakan");
+            list.Add(12.5f);
+            list.Add(false);
+            list.Add('i');
+
+            string[] Comps = {"Lenovo", "Asus", "AMD", "Intel"};
+
+            list.AddRange(Comps);
+            
+
+
+            list.Clear();
+            List<int> listAdd = new List<int>(){5,2,12,6,3,8};
+            list.AddRange(listAdd);
+
+            list.Sort();
+
+
+            Console.WriteLine("Binary Search 3.Index: " + list.BinarySearch(3));
+
+            list.Reverse();
+            
+            for(int i = 0; i < list.Count; i++)
+                Console.WriteLine($"{i}.index: {list[i]}");
+        }
+    }
+}
