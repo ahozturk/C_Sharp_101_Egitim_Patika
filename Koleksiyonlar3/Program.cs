61935f3dc9097914d64067ca1d20cd516056bc65
Hakan <ahmethakanozturk34@gmail.com>
2022-05-11 22:03:29 +0300

    Collections3

---
+++ Koleksiyonlar3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> dict = new Dictionary<int, string>();
+            dict.Add(1, "Hakan");
+            dict.Add(2, "Yavuz");
+            dict.Add(5, "Tuana");
+            
+            foreach (var a in dict)
+                Console.WriteLine(a);
+            
+            Console.WriteLine($"Dictionary item count: {dict.Count}");
+            Console.WriteLine($"Is there 3 in key: {dict.ContainsKey(3)}");
+
+
+        }
+    }
+}
