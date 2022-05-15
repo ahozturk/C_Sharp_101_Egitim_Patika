bbf1ae06b781560a9a4444c855bc410605db0c4b
Hakan <ahmethakanozturk34@gmail.com>
2022-05-15 13:37:59 +0300

    Collections-Quest-3

---
+++ KoleksiyonlarOdev/Koleksiyonlar-Soru-3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a sentences: ");
+            string input = Console.ReadLine();
+            char[] vowelChars = new char[] {'e', 'u', 'o', 'a', 'i', 'ü', 'ı', 'ö'};
+
+            List<char> vowels = new List<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if(vowelChars.Contains(Convert.ToChar(input[i].ToString().ToLower())))
+                {
+                    vowels.Add(input[i]);
+                    Console.WriteLine("Added: " + input[i]);
+
+                }
+            }
+            
+            for(int i = 0; i < vowels.Count; i++)
+            {
+                Console.WriteLine(vowels[i]);
+            }
+        }
+    }
+}
